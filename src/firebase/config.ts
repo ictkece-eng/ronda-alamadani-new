a3efa7e4c15f6f2ae1e94240b3aa13e86cedf784
perum alam madani <perumalammadani@gmail.com>
2026-01-30 11:26:27 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3050413614-a317d",
+  "appId": "1:303744619634:web:13e9b078b46e0fe860deeb",
+  "apiKey": "AIzaSyAkc0gCiYIxG3G9kJ4AMFlOAZi_4XmDNrg",
+  "authDomain": "studio-3050413614-a317d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "303744619634"
+};
