fdbfc60a20983279058933ec384e4f5bc74c09a3
perum alam madani <perumalammadani@gmail.com>
2026-01-30 12:46:04 +0000

    const firebaseConfig = {   apiKey: "AIzaSyAkc0gCiYIxG3G9kJ4AMFlOAZi_4XmD

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
 export const firebaseConfig = {
-  "apiKey": "AIzaSyA80qYZ5rH12ri-JYl6Vp_V38Bl1jPtHaI",
-  "authDomain": "ronda-alammadani-33b2d.firebaseapp.com",
-  "projectId": "ronda-alammadani-33b2d",
-  "storageBucket": "ronda-alammadani-33b2d.firebasestorage.app",
-  "messagingSenderId": "29682031241",
-  "appId": "1:29682031241:web:7033fe51a5b5500c7ac458",
-  "measurementId": "G-NRW9VD03L6"
+  "apiKey": "AIzaSyAkc0gCiYIxG3G9kJ4AMFlOAZi_4XmDNrg",
+  "authDomain": "studio-3050413614-a317d.firebaseapp.com",
+  "projectId": "studio-3050413614-a317d",
+  "storageBucket": "studio-3050413614-a317d.firebasestorage.app",
+  "messagingSenderId": "303744619634",
+  "appId": "1:303744619634:web:13e9b078b46e0fe860deeb"
 };
