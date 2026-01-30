64f5e9098200f6eb8406b59f919b1b51d65a43ef
perum alam madani <perumalammadani@gmail.com>
2026-01-30 12:01:01 +0000

    masih belum bisa login saya bro

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   "apiKey": "AIzaSyA80qYZ5rH12ri-JYl6Vp_V38Bl1jPtHaI",
   "authDomain": "ronda-alammadani-33b2d.firebaseapp.com",
   "projectId": "ronda-alammadani-33b2d",
-  "storageBucket": "ronda-alammadani-33b2d.appspot.com",
+  "storageBucket": "ronda-alammadani-33b2d.firebasestorage.app",
   "messagingSenderId": "29682031241",
   "appId": "1:29682031241:web:7033fe51a5b5500c7ac458",
   "measurementId": "G-NRW9VD03L6"
